9ea9dd817e99bd6ea9e872b8e326a577aaff24ba
okaki1999 <okakilikecat@gmail.com>
2024-12-28 17:32:59 +0900

    コントローラーの作成

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UtilityController;
+use App\Http\Controllers\GameController;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -13,43 +15,11 @@ Route::get('/curriculum', fn() => view('curriculum'));
 Route::get('/hello', fn() => view('hello',['name' => 'taiki','course' => 'laravel']));
 
 // 世界の時間
-Route::get('/world-time', function () {
-    $timeDiff = [
-        '東京' => 0,
-        'シンガポール' => -1,
-        'パリ' => -8,
-        'ロンドン' => -9,
-        'ニューヨーク' => -14,
-        'ロサンゼルス' => -17,
-        'ハワイ' => -19,
-    ];
-    $times = array_map(fn($diff) => now()->addHours($diff), $timeDiff);
-    return view('world-time', ['times' => $times]);
-});
+Route::get('/world-time', [UtilityController::class, 'worldTime']);
 
 // おみくじ
-Route::get('/omikuji', function () {
-    $fortunes = ['大吉', '中吉', '小吉', '吉', '末吉', '凶', '大凶'];
-    $resultIndex = array_rand($fortunes);
-    $result = $fortunes[$resultIndex];
-    return view('omikuji', ['result' => $result]);
-});
+Route::get('/omikuji', [GameController::class, 'omikuji']);
 
 // モンティ・ホール問題
-Route::get('/monty-hall', function () {
-    $results = [];
-    for ($i = 0; $i < 1000; $i++) {
-        $options = [true, false, false];
-        shuffle($options);
+Route::get('/monty-hall', [GameController::class, 'montyHall']);
 
-        $selectedIndex = array_rand($options);
-        $notSelectedIndexes = array_filter($options, fn($index) => $index !== $selectedIndex, ARRAY_FILTER_USE_KEY);
-        $removeIndex = array_search(false, $notSelectedIndexes);
-        unset($notSelectedIndexes[$removeIndex]);
-
-        $changedIndex = key($notSelectedIndexes);
-        $results[] = $options[$changedIndex];
-    }
-    $wonCount = count(array_filter($results, fn($result) => $result));
-    return view('monty-hall', ['results' => $results, 'wonCount' => $wonCount]);
-});
